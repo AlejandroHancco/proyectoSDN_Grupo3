14eab8bf2d788b391e0d2d9ffa9a32085f878a3b
AlejandroHancco <142966841+AlejandroHancco@users.noreply.github.com>
2025-07-01 14:16:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -40,5 +40,23 @@ def logout():
     session.clear()
     return redirect(url_for("login"))
 
+# Editar curso
+@app.route("/editar_curso/<int:idcurso>", methods=["GET", "POST"])
+def editar_curso(idcurso):
+    if request.method == "POST":
+        nombre = request.form.get("nombre")
+        estado = request.form.get("estado")
+        repository.actualizar_curso(idcurso, nombre, estado)
+        return redirect(url_for("login"))  # Redirige a login o a donde veas el admin
+    curso = repository.get_curso_por_id(idcurso)
+    return render_template("editarCurso.html", curso=curso)
+
+# Eliminar curso
+@app.route("/eliminar_curso/<int:idcurso>")
+def eliminar_curso(idcurso):
+    repository.eliminar_curso(idcurso)
+    return redirect(url_for("login"))
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=30000, debug=True)
