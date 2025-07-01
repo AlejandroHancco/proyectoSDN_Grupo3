4f483a4b96ca62264731b0f17e815c91868667cb
AlejandroHancco <142966841+AlejandroHancco@users.noreply.github.com>
2025-07-01 13:19:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,82 +1,17 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template, request, session, redirect, url_for
-from pyrad.client import Client
-from pyrad.dictionary import Dictionary
-from pyrad.packet import AccessRequest, AccessAccept
-import mysql.connector
+from files import repository
 
 app = Flask(__name__)
-app.secret_key = "grupo4"
-
-# Configuración MySQL
-db_config = {
-    "host": "192.168.201.200",
-    "user": "grupo3",
-    "password": "grupo3",
-    "database": "sdnG03_db"
-}
-
-# Configuración FreeRADIUS
-RADIUS_SERVER = "127.0.0.1"
-RADIUS_PORT = 1812
-SECRET = b"testing123"
-DICT_PATH = "dictionary"
-
-client = Client(server=RADIUS_SERVER, secret=SECRET, dict=Dictionary(DICT_PATH))
-client.AuthPort = RADIUS_PORT
-
-def authenticateUser(username, password):
-    req = client.CreateAuthPacket(code=AccessRequest, User_Name=username)
-    req["User-Password"] = req.PwCrypt(password)
-    reply = client.SendPacket(req)
-    return reply.code == AccessAccept
-
-def getUserDb(username):
-    try:
-        conn = mysql.connector.connect(**db_config)
-        cursor = conn.cursor(dictionary=True)
-        query = """
-            SELECT u.username, u.names, u.lastnames, u.code, u.rol, r.rolname
-            FROM user u
-            JOIN role r ON u.rol = r.idrole
-            WHERE u.username = %s
-        """
-        cursor.execute(query, (username,))
-        user = cursor.fetchone()
-        cursor.close()
-        conn.close()
-        return user
-    except Exception as e:
-        print(f"DB error: {e}")
-        return None
-
-def getCursosAlumno(username):
-    try:
-        conn = mysql.connector.connect(**db_config)
-        cursor = conn.cursor(dictionary=True)
-        query = """
-            SELECT c.idcurso, c.nombre, c.estado
-            FROM curso c
-            JOIN inscripcion i ON c.idcurso = i.curso_idcurso
-            JOIN user u ON i.user_iduser = u.iduser
-            WHERE u.username = %s AND c.estado = 'activo'
-        """
-        cursor.execute(query, (username,))
-        cursos = cursor.fetchall()
-        cursor.close()
-        conn.close()
-        return cursos
-    except Exception as e:
-        print(f"DB error cursos: {e}")
-        return []
+app.secret_key = "grupo3"
 
 @app.route("/", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         username = request.form.get("username")
         password = request.form.get("password")
-        if authenticateUser(username, password):
-            usuario = getUserDb(username)
+        if repository.authenticate_user(username, password):
+            usuario = repository.get_user_db(username)
             if not usuario:
                 return "Usuario no encontrado en BD", 404
             session["usuario"] = usuario
@@ -84,15 +19,14 @@ def login():
             rol = usuario["rolname"].lower()
 
             if rol == "alumno":
-                cursos = getCursosAlumno(username)
+                cursos = repository.get_cursos_alumno(username)
                 return render_template("cursos.html", usuario=usuario, cursos=cursos)
             elif rol == "profesor":
                 return render_template("profesorPrincipal.html", usuario=usuario)
             elif rol == "administrador":
-                usuarios = getAllUsuarios()
-                cursos = getAllCursos()
+                usuarios = repository.get_all_usuarios()
+                cursos = repository.get_all_cursos()
                 return render_template("adminPrincipal.html", usuario=usuario, usuarios=usuarios, cursos=cursos)
-
             elif rol == "invitado":
                 return render_template("invitadoPrincipal.html", usuario=usuario)
             else:
@@ -101,42 +35,10 @@ def login():
             return "Credenciales inválidas", 401
     return render_template("login.html")
 
-
-
 @app.route("/logout")
 def logout():
     session.clear()
     return redirect(url_for("login"))
-
-def getAllUsuarios():
-    try:
-        conn = mysql.connector.connect(**db_config)
-        cursor = conn.cursor(dictionary=True)
-        cursor.execute("""
-            SELECT u.username, u.names, u.lastnames, r.rolname
-            FROM user u
-            JOIN role r ON u.rol = r.idrole
-        """)
-        usuarios = cursor.fetchall()
-        cursor.close()
-        conn.close()
-        return usuarios
-    except Exception as e:
-        print(f"DB error usuarios: {e}")
-        return []
-
-def getAllCursos():
-    try:
-        conn = mysql.connector.connect(**db_config)
-        cursor = conn.cursor(dictionary=True)
-        cursor.execute("SELECT idcurso, nombre, estado FROM curso")
-        cursos = cursor.fetchall()
-        cursor.close()
-        conn.close()
-        return cursos
-    except Exception as e:
-        print(f"DB error cursos: {e}")
-        return []
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=30000, debug=True)
